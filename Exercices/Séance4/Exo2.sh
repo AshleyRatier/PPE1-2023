835bad86de326817946156d36673065304f600c9
Ashley Ratier <ashley.ratier@gmail.com>
2023-10-22 15:33:19 +0200

    Ajout validation dans Exo2.sh

--- Exercices/Séance4/Exo2.sh
+++ Exercices/Séance4/Exo2.sh
@@ -4,4 +4,13 @@
 # mois = $2
 # nombre de lieux à afficher = $3
 
-Cat ./$1/$2/* | grep Location | cut -f 3 | head -n $3 | sort | uniq -c | sort -nr
+if [[ $1 =~ "2016"|"2017"|"2018" ]] &&
+[[ $2 =~ "01"|"02"|"03"|"04"|"05"|"06"|"07"|"08"|"09"|"10"|"11"|"12" ]] &&
+[[ $3 -gt 0 ]]
+then
+	echo "L'argument est valide."
+	cat ./$1/$2/* | grep Location | cut -f 3 | head -n $3 | sort | uniq -c | sort -nr
+
+else
+	exit
+fi
