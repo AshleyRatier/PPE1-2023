5bd56aef71e98530f4b6552d996becd401b7468b
Ashley Ratier <ashley.ratier@gmail.com>
2023-10-22 14:31:27 +0200

    Ajout validation dans Exo1Q1.sh

--- Exercices/Séance4/Exo1Q1.sh
+++ Exercices/Séance4/Exo1Q1.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 
-cat ./2016/*/* | grep $1 | wc -l
+if [[ $1 =~ "Location"|"Person"|"Date"|"Hour"|"Organization"|"Product"|"Event" ]]
+then 
+	echo "L'argument est valide."
+	cat ./2016/*/* | grep $1 | wc -l
+else
+	exit
+fi
