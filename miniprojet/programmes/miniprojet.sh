5802984c596f4be528b5826893380c900358ce0f
Ashley Ratier <ashley.ratier@gmail.com>
2023-11-05 12:27:05 +0100

    AJout miniprojet + Journal Séance6

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,7 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash 
+
+urlfile=$1
+lineno=1
 
 while read -r line;
-do
-	echo $line;
-done < $2;
+do 
+    codehttp=$(curl -s -i "$line" | grep "HTTP")
+    
+    if [[ $codehttp == *" 301 "* ]]; 
+    then 
+        line=$(curl -s -i -L "$line" | grep "Location:.*" | cut -d ' ' -f 2)
+        codehttp="HTTP/2 200"      
+    fi
 
+    encodage=$(curl -s -i "$line" | grep "charset" | sed 's/.*charset=//')
+    echo -e "${lineno}\t${line}\t${codehttp}\t${encodage}"
+    lineno=$(expr $lineno + 1)
+	
+done < $urlfile
